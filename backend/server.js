4d0781212f596ceff3ce1a98e150f051cd816f60
Ashish Mittal <aashishrbmittal@gmail.com>
2025-06-12 20:10:23 +0530

    onboard logic added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/authRoute.js'
 import { connectDB } from './config/db.js'
+import cookieParser from "cookie-parser";
 
 dotenv.config()
 
@@ -9,6 +10,7 @@ const app = express()
 const PORT = process.env.PORT
 
 app.use(express.json())
+app.use(cookieParser());
 
 app.get('/', (req,res) => {
     res.send("Hello World!")
